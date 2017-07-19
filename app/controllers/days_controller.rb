240f88b95016a35236fd25a5f74680e165c0ebf6
jesse calvillo <jesse.calvillo@gmail.com>
2017-07-19 02:32:52 -0700

    json parsing for directions API fixed

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -8,28 +8,28 @@ class DaysController < ApplicationController
    end
 
    def show
-      @date = Day.includes(:appointments).includes(:locations).find(params[:id])
-      @locations = Location.all
-      @time = 0
-      origin = ERB::Util.url_encode("36.058241, -119.053045")
-      destinations = @date.locations.where(id: @date.appointments.pluck(:location_id)).map{|x| [x.latitude, x.longitude].join(',')}.join('|')
-      dest = ERB::Util.url_encode(destinations)
-      url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{origin}&waypoints=optimize:true|#{dest}&key=AIzaSyAtEhW3fIf9xkfpRZ8f_HEkV3pk2zxqsvI"
-      uri = URI(url)
-      unless destinations.blank?
+    @date = Day.includes(:appointments).includes(:locations).find(params[:id])
+    @locations = Location.all
+    @time = 0.00
+    origin = ERB::Util.url_encode("36.058241, -119.053045")
+    destinations = @date.locations.where(id: @date.appointments.pluck(:location_id)).map{|x| [x.latitude, x.longitude].join(',')}.join('|')
+    dest = ERB::Util.url_encode(destinations)
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{origin}&waypoints=optimize:true|#{dest}&key=AIzaSyAtEhW3fIf9xkfpRZ8f_HEkV3pk2zxqsvI"
+    uri = URI(url)
+    unless destinations.blank?
       dist_json = ActiveSupport::JSON.decode(Net::HTTP.get(uri))
-        dist_json["routes"].each do |section|
-          section["legs"].each do |json|
-            duration = json["duration"]["value"] if json["duration"] != nil
-            if duration != nil
-              duration = duration/(60*60)
-              @time = duration + @time
-            end
-          end
+      dist_json["routes"].each do |section|
+        section["legs"].each do |json|
+          duration = json["duration"]["value"] if json["duration"] != nil
+          @time = duration + @time
         end
       end
-     end
-     
+    end
+    if @time != nil
+      @time = @time/(60*60)
+    end
+   end
+
 
    def new
      @date = Day.new
